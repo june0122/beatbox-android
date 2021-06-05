09c86389e56bf34ac6a0ce398e7d9a1232d48369
june0122 <june0122@naver.com>
2021-06-05 22:35:41 +0900

    feat: SoundPool 생성 및 음원 로드와 재생 설정

--- app/src/main/java/com/june0122/beatbox/BeatBox.kt
+++ app/src/main/java/com/june0122/beatbox/BeatBox.kt
@@ -1,29 +1,38 @@
 package com.june0122.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 import java.lang.Exception
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 class BeatBox(private val assets: AssetManager) {
 
     val sounds: List<Sound>
+    private val soundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
 
     init {
         sounds = loadSounds()
     }
 
-    //  fun loadSounds() : List<String> {
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
+        }
+    }
+
     private fun loadSounds(): List<Sound> {
         val soundNames: Array<String>
 
         try {
             soundNames = assets.list(SOUNDS_FOLDER)!!
-//            val soundNames = assets.list(SOUNDS_FOLDER)!!
-//            Log.d(TAG, "Found ${soundNames.size} sounds")
-//            return soundNames.asList()
         } catch (e: Exception) {
             Log.e(TAG, "Could not list assets", e)
             return emptyList()
@@ -32,8 +41,20 @@ class BeatBox(private val assets: AssetManager) {
         soundNames.forEach { fileName ->
             val assetPath = "$SOUNDS_FOLDER/$fileName"
             val sound = Sound(assetPath)
-            sounds.add(sound)
+//            sounds.add(sound)
+            try {
+                load(sound)
+                sounds.add(sound)
+            } catch (ioe: IOException) {
+                Log.e(TAG, "Could not load sound $fileName", ioe)
+            }
         }
         return sounds
+    }
+
+    private fun load(sound: Sound) {
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
     }
 }
--- app/src/main/java/com/june0122/beatbox/Sound.kt
+++ app/src/main/java/com/june0122/beatbox/Sound.kt
@@ -2,7 +2,6 @@ package com.june0122.beatbox
 
 private const val WAV = ".wav"
 
-class Sound(val assetPath: String) {
-
+class Sound(val assetPath: String, var soundId: Int? = null) {
     val name = assetPath.split("/").last().removeSuffix(WAV)
 }
